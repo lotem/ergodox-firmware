13ac1af17fb63c30e22f6e119cbec15c7673b456
Chen Gong <chen.sst@gmail.com>
2016-06-29 17:06:24 +0800

    Big control and meta keys.

--- src/keyboard/ergodox/layout/kbcon-layout.h
+++ src/keyboard/ergodox/layout/kbcon-layout.h
@@ -9,19 +9,19 @@ ERGODOX_LAYOUT = {
     _(_tab),        _(_Q),          _(_W),          _(_E),          _(_R),          _(_T),          _(_bracketL),
     _(_backslash),  _(_A),          _(_S),          _(_D),          _(_F),          _(_G),
     S2CAP(_shiftL), _(_Z),          _(_X),          _(_C),          _(_V),          _(_B),          _(_dash),
-    _(_ctrlL),      _(_altL),       _(_shiftL),     _(_guiL),       CHORD(1),
+    _(_ctrlL),      _(_altL),       _(_guiL),       S2CAP(_shiftL), CHORD(1),
                                                                                     _(_arrowL),     _(_arrowR),
                                                                                                     _(_grave),
-                                                                    _(_space),      _(_shiftL),     _(_tab),
+                                                                    _(_space),      _(_ctrlL),      _(_tab),
     // right hand
     CHORD(9),       _(_6),          _(_7),          _(_8),          _(_9),          _(_0),          _(_enter),
     _(_bracketR),   _(_Y),          _(_U),          _(_I),          _(_O),          _(_P),          _(_bs),
     /*_*/           _(_H),          _(_J),          _(_K),          _(_L),          _(_semicolon),  _(_quote),
     _(_equal),      _(_N),          _(_M),          _(_comma),      _(_period),     _(_slash),      S2CAP(_shiftR),
-                                    CHORD(1),       _(_guiR),       _(_shiftR),     _(_altR),       _(_ctrlR),
+                                    CHORD(1),       S2CAP(_shiftR), _(_guiR),       _(_altR),       _(_ctrlR),
     _(_arrowD),     _(_arrowU),
     _(_del),
-    _(_bs),         _(_ctrlR),      _(_enter)
+    _(_bs),         _(_guiR),       _(_enter)
   ),
   ERGODOX_LAYER( // L1: symbols and function
     // left hand
