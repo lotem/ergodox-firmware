91109e901a7070dc43db41a64eab04fd6daee331
Chen Gong <chen.sst@gmail.com>
2016-08-05 02:31:05 +0800

    Add Minimak layout and redesigned symbols layer.

--- src/keyboard/ergodox/layout/kbcon-layout.h
+++ src/keyboard/ergodox/layout/kbcon-layout.h
@@ -5,45 +5,45 @@
 ERGODOX_LAYOUT = {
   ERGODOX_LAYER( // L0: programming
     // left hand
-    _(_esc),        _(_1),          _(_2),          _(_3),          _(_4),          _(_5),          CHORD(9),
+    _(_esc),        _(_1),          _(_2),          _(_3),          _(_4),          _(_5),          _(_grave),
     _(_tab),        _(_Q),          _(_W),          _(_E),          _(_R),          _(_T),          _(_bracketL),
-    _(_backslash),  _(_A),          _(_S),          _(_D),          _(_F),          _(_G),
+    _(_ctrlL),      _(_A),          _(_S),          _(_D),          _(_F),          _(_G),
     S2CAP(_shiftL), _(_Z),          _(_X),          _(_C),          _(_V),          _(_B),          _(_dash),
-    _(_ctrlL),      _(_altL),       S2CAP(_shiftL), _(_guiL),       CHORD(1),
-                                                                                    _(_tab),        _(_grave),
-                                                                                                    _(_altL),
-                                                                    _(_space),      _(_ctrlL),      _(_guiL),
+    _(_guiL),       _(_altL),       _(_bracketL),   _(_bracketR),   CHORD(1),
+                                                                                    _(_altL),       _(_guiL),
+                                                                                                    _(_shiftL),
+                                                                    _(_space),      _(_tab),        _(_ctrlL),
     // right hand
-    CHORD(9),       _(_6),          _(_7),          _(_8),          _(_9),          _(_0),          _(_enter),
+    _(_backslash),  _(_6),          _(_7),          _(_8),          _(_9),          _(_0),          _(_enter),
     _(_bracketR),   _(_Y),          _(_U),          _(_I),          _(_O),          _(_P),          _(_bs),
     /*_*/           _(_H),          _(_J),          _(_K),          _(_L),          _(_semicolon),  _(_quote),
     _(_equal),      _(_N),          _(_M),          _(_comma),      _(_period),     _(_slash),      S2CAP(_shiftR),
-                                    CHORD(1),       _(_guiR),       S2CAP(_shiftR), _(_altR),       _(_ctrlR),
-    _(_del),        _(_bs),
-    _(_altR),
-    _(_guiR),       _(_shiftR),     _(_enter)
+                                    CHORD(1),       _(_dash),       _(_equal),      _(_altR),       _(_ctrlR),
+    _(_ctrlR),      _(_altR),
+    _(_shiftR),
+    _(_guiR),       _(_bs),         _(_enter)
   ),
   ERGODOX_LAYER( // L1: symbols and function
     // left hand
     _(_insert),     _(_F1),         _(_F2),         _(_F3),         _(_F4),         _(_F5),         _(_F11),
-    M(PREV_TRACK),  S(_1),          S(_2),          S(_3),          S(_4),          S(_5),          __,
-    M(PLAY_PAUSE),  S(_backslash),  _(_bracketL),   _(_bracketR),   S(_dash),       S(_bracketL),
-    M(NEXT_TRACK),  _(_1),          _(_2),          _(_3),          _(_4),          _(_5),          __,
-    __,             __,             __,             __,             _(_tab),
+    M(PREV_TRACK),  _(_equal),      S(_equal),      _(_bracketL),   _(_bracketR),   _(_grave),      __,
+    M(PLAY_PAUSE),  S(_2),          _(_backslash),  S(_9),          S(_0),          S(_7),
+    M(NEXT_TRACK),  S(_5),          S(_8),          S(_bracketL),   S(_bracketR),   S(_backslash),  __,
+    __,             __,             __,             __,             CHORD(9),
                                                                                     __,             __,
                                                                                                     __,
                                                                     __,             __,             __,
     // right hand
     _(_F12),        _(_F6),         _(_F7),         _(_F8),         _(_F9),         _(_F10),        _(_del),
-    __,             S(_6),          S(_7),          S(_8),          S(_grave),      _(_grave),      _(_volumeU),
-    /*_*/           S(_bracketR),   S(_equal),      S(_9),          S(_0),          S(_quote),      _(_volumeD),
-    __,             _(_6),          _(_7),          _(_8),          _(_9),          _(_0),          _(_mute),
-                                    _(_bs),         _(_arrowL),     _(_arrowD),     _(_arrowU),     _(_arrowR),
+    __,             S(_6),          _(_bs),         _(_arrowU),     _(_tab),        S(_1),          _(_volumeU),
+    /*_*/           S(_3),          _(_arrowL),     _(_arrowD),     _(_arrowR),     _(_quote),      _(_mute),
+    __,             S(_grave),      S(_dash),       S(_4),          _(_dash),       S(_quote),      _(_volumeD),
+                                    CHORD(9),       __,             __,             __,             __,
     _(_home),       _(_end),
     _(_pageU),
-    _(_pageD),      __,             _(_enter)
+    _(_pageD),      _(_del),        __
   ),
-  ERGODOX_LAYER( // L2: PC
+  ERGODOX_LAYER( // L2: custom
     // left hand
     __,             __,             __,             __,             __,             __,             __,
     __,             __,             __,             __,             __,             __,             __,
@@ -51,8 +51,8 @@ ERGODOX_LAYOUT = {
     __,             __,             __,             __,             __,             __,             __,
     __,             __,             __,             __,             __,
                                                                                     __,             __,
-                                                                                                    _(_guiL),
-                                                                    __,             _(_ctrlL),      _(_altL),
+                                                                                                    __,
+                                                                    __,             __,             __,
     // right hand
     __,             __,             __,             __,             __,             __,             __,
     __,             __,             __,             __,             __,             __,             __,
@@ -60,28 +60,28 @@ ERGODOX_LAYOUT = {
     __,             __,             __,             __,             __,             __,             __,
                                     __,             __,             __,             __,             __,
     __,             __,
-    _(_guiR),
-    _(_altR),       _(_shiftR),     __
+    __,
+    __,             __,             __
   ),
-  ERGODOX_LAYER( // L3: typewriter
+  ERGODOX_LAYER( // L3: Minimak
     // left hand
-    __,             __,             __,             __,             __,             __,             __,
-    __,             __,             __,             __,             __,             __,             __,
-    __,             __,             __,             __,             __,             __,
-    __,             __,             __,             __,             __,             __,             __,
+    __,             _(_1),          _(_2),          _(_3),          _(_4),          _(_5),          __,
+    __,             _(_Q),          _(_W),          _(_D),          _(_F),          _(_K),          __,
+    __,             _(_A),          _(_S),          _(_T),          _(_R),          _(_G),
+    __,             _(_Z),          _(_X),          _(_C),          _(_V),          _(_B),          __,
     __,             __,             __,             __,             __,
                                                                                     __,             __,
-                                                                                                    _(_altL),
-                                                                    __,             _(_shiftL),     _(_ctrlL),
+                                                                                                    __,
+                                                                    __,             __,             __,
     // right hand
-    __,             __,             __,             __,             __,             __,             __,
-    __,             __,             __,             __,             __,             __,             __,
-    /*_*/           __,             __,             __,             __,             __,             __,
-    __,             __,             __,             __,             __,             __,             __,
+    __,             _(_6),          _(_7),          _(_8),          _(_9),          _(_0),          __,
+    __,             _(_Y),          _(_U),          _(_I),          _(_L),          _(_semicolon),  __,
+    /*_*/           _(_H),          _(_N),          _(_E),          _(_O),          _(_P),          __,
+    __,             _(_J),          _(_M),          _(_comma),      _(_period),     _(_slash),      __,
                                     __,             __,             __,             __,             __,
     __,             __,
-    _(_altR),
-    _(_guiR),       _(_bs),         __
+    __,
+    __,             __,             __
   ),
   ERGODOX_LAYER( // L4: Maltron
     // left hand
